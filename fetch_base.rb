784af1c57aa7f73e209ed36c5f44e81a51a6737d
Jeff Miller <jeffmiller00@gmail.com>
2017-11-29 18:47:08 -0600

    Refactor to DRY up the code and remove BMW as an option.

---
+++ fetch_base.rb
@@ -0,0 +1,52 @@
+require 'httpi'
+require 'nokogiri'
+require 'watir'
+require 'pry'
+require 'google_drive'
+
+
+MAKES = %w(Ford Mazda)
+MODELS = %w(Edge CX5 CX-5)
+MAX_MILES = 50000
+MIN_PRICE = 20000
+MAX_PRICE = 32000
+PER_PAGE = 36
+
+`brew services start chromedriver`
+
+
+def interested?(model)
+  !model.nil?
+end
+
+def get_model(car_text)
+  car_text.strip.split.find { |hay| MODELS.include?(hay)}
+end
+
+def get_year(car_text)
+  year = car_text[/\d{4}/].to_i
+  return year if year > 2000
+
+  year = car_text[/\d{2}/]
+  year.to_i + 2000
+end
+
+def get_make(car_text)
+  car_text.strip.split.find { |hay| MAKES.include?(hay)}
+end
+
+def get_mpgs(mpg_txt)
+  mpgs = mpg_txt.split('/')
+  mpgs.each_with_index do |mpg, i|
+    mpgs[i] = mpg.gsub(/\D/, "").to_i
+  end
+  mpgs
+end
+
+def get_worksheet
+  # Authenticate a session with your Service Account
+  session = GoogleDrive::Session.from_service_account_key("client_secret.json")
+  spreadsheet = session.spreadsheet_by_title('Car Finder')
+  worksheet = spreadsheet.worksheets.first
+  worksheet
+end
